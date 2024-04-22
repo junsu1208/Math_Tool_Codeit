b2c2d4a37131911389b2e0920a18b8df08f5267b
Jin Junsu <junsu1208@naver.com>
2024-04-22 12:18:17 +0900

    Add factorial function

--- calculator.py
+++ calculator.py
@@ -27,4 +27,11 @@ def get_Percent(a, b)
   return (a / b) * 100
 
 def get_Sum(n):
-    return n(n + 1) / 2
+  return n(n + 1) / 2
+
+def factorial(n):
+  num = 1
+  while n >= 1:
+    num = num * n
+    n = n - 1
+  return num
